eed4ad99275f7bbc3f16f71174f09a54d48459b9
Derek <darmfield2023@gmail.com>
2023-02-19 07:34:44 -0500

    created mock patient queries

--- test.js
+++ test.js
@@ -1,17 +1,42 @@
-const { Block, BlockChain } = require("./blockchain.js");
+const { Block, BlockChain, Transaction } = require("./blockchain.js");
+const EC = require("elliptic").ec, ec = new EC("secp256k1");
+
 
 const MediChain = new BlockChain();
 
+
+const hospital = ec.genKeyPair();
+const patient1 = ec.genKeyPair();
+const patient2 = ec.genKeyPair();
+
+//  - - - - - - -- - -- - block 1 - - - - - - -- - -- - 
+
 json_data = {Height: "5'8", EyeColour: "Hazel", Gender: "Male"}
-
-block1 = new Block(Date.now().toString(), json_data);
-MediChain.addBlock(block1);
-
-json_data = {Height: "1'23", EyeColour: "Brown", Gender: "Male"}
-block2 = new Block(Date.now().toString(), json_data);
-
-MediChain.addBlock(block2);
-
-console.log(MediChain.chain)
+const transaction1 = new Transaction(hospital.getPublic("hex"), patient1.getPublic("hex"), Date.now().toString(), json_data);
+transaction1.sign(hospital);
+MediChain.addTransaction(transaction1);
 
 
+json_data2 = {Height: "5'4", EyeColour: "Brown", Gender: "Male"}
+const transaction2 = new Transaction(hospital.getPublic("hex"), patient2.getPublic("hex"), Date.now().toString(), json_data2);
+transaction2.sign(hospital);
+MediChain.addTransaction(transaction2);
+
+MediChain.mineTransactions();
+
+//  - - - - - - -- - -- - block 1 - - - - - - -- - -- - 
+
+//  - - - - - - -- - -- - block 2 - - - - - - -- - -- - 
+
+// Create a transaction
+const transaction3 = new Transaction(hospital.getPublic("hex"), patient1.getPublic("hex"), Date.now().toString(), {data: 'Diagnosed patient with OCD'});
+transaction3.sign(hospital);
+MediChain.addTransaction(transaction3);
+
+// Mine transaction
+MediChain.mineTransactions();
+
+// Prints out balance of both address
+console.log("Hospital Data:\n" + MediChain.getReport(hospital.getPublic("hex")));
+console.log("Patient1 Data:\n" + MediChain.getReport(patient1.getPublic("hex")));
+console.log("Patient2 Data:\n" + MediChain.getReport(patient2.getPublic("hex")));
