14fb554ff3f1d4da6538f750b93142596cee5842
Derek <darmfield2023@gmail.com>
2023-02-19 12:11:31 -0500

    enabled API functionality

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,21 +1,37 @@
 var express = require("express");
+const { userInfo } = require("os");
+const { stringify } = require("querystring");
 var router = express.Router();
+
+var playground = require('./blockchain/playground');
+let keyPair;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/regist", function (req, res, next) {
-  res.send("This is to register a new user");
+router.get("/createUser", function (req, res, next) {
+  keyPair = playground.createUser()
+  const public = keyPair.getPublic('hex');
+  res.send(public);
+
 });
 
-router.get("/login", function (req, res, next) {
-  res.send("this is to login a user");
+router.get("/viewReport/:publicKey", function (req, res, next) {
+  const publicKey = req.params['publicKey'];
+  report = playground.viewReport(publicKey);
+  res.send(report);
 });
 
-router.get("send", function (req, res, next) {
-  res.send("this is to send a file");
+router.get("/createTransaction/:from/:to/:entry", function (req, res, next) {
+  const from = req.params['from'];
+  const to = req.params['to'];
+  const entry = req.params['entry'];
+
+  playground.createTransaction(keyPair, to, entry);
+  res.send('transaction complete');
 });
+
 
 module.exports = router;
